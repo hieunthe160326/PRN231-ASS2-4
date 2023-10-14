7e0a9558382afcae7b3547040cc087de1b53e28b
hungvietmai <hungmvhe163655@fpt.edu.vn>
2023-10-14 16:20:57 +0700

    Add Order Detail Controller

---
+++ SE1623_Group4_A2/eStoreAPI/Controllers/OrderDetailsController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using eStoreAPI.Models;
+using Microsoft.AspNetCore.OData.Formatter;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+namespace eStoreAPI.Controllers
+{
+    [Route("odata/OrderDetails")]
+    public class OrderDetailsODataController : ODataController
+    {
+        private readonly EStoreContext _context;
+
+        public OrderDetailsODataController(EStoreContext context)
+        {
+            _context = context;
+        }
+
+        [EnableQuery]
+        public IActionResult Get()
+        {
+            return Ok(_context.OrderDetails);
+        }
+
+        [EnableQuery]
+        public IActionResult Get([FromODataUri] int key)
+        {
+            var orderDetail = _context.OrderDetails.FirstOrDefault(o => o.OrderDetailId == key);
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+            return Ok(orderDetail);
+        }
+
+        [HttpPost]
+        [EnableQuery]
+        public IActionResult Post([FromBody] OrderDetail orderDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.OrderDetails.Add(orderDetail);
+            _context.SaveChanges();
+            return Created(orderDetail);
+        }
+
+        [HttpPut]
+        [EnableQuery]
+        public IActionResult Put([FromODataUri] int key, [FromBody] OrderDetail orderDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (key != orderDetail.OrderDetailId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(orderDetail).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Updated(orderDetail);
+        }
+
+        [HttpDelete]
+        [EnableQuery]
+        public IActionResult Delete([FromODataUri] int key)
+        {
+            var orderDetail = _context.OrderDetails.Find(key);
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrderDetails.Remove(orderDetail);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
+
--- SE1623_Group4_A2/eStoreAPI/Program.cs
+++ SE1623_Group4_A2/eStoreAPI/Program.cs
@@ -1,14 +1,45 @@
+using eStoreAPI.Models;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
 namespace eStoreAPI
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            //Get EDM 
+            ODataConventionModelBuilder edmBuilder = new ODataConventionModelBuilder();
+            edmBuilder.EntitySet<Category>("Category");
+            edmBuilder.EntitySet<Member>("Member");
+            edmBuilder.EntitySet<Order>("Order");
+            edmBuilder.EntitySet<OrderDetail>("OrderDetail");
+            edmBuilder.EntitySet<Product>("Product");
+
+            IEdmModel edmModel = edmBuilder.GetEdmModel();
+
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            //Add Service InMemoryData
+            builder.Services.AddDbContext<EStoreContext>();
 
+            // Add services to the container.
             builder.Services.AddControllers();
+
+            //Add Service Odata
+            builder.Services.AddControllers().AddOData(opt
+                => opt.Select().Filter().Count().OrderBy().Expand().SetMaxTop(10)
+                .AddRouteComponents("odata", edmModel));
+
+            //Setup for json parsing
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                options.JsonSerializerOptions.DictionaryKeyPolicy = null;
+            });
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -24,6 +55,7 @@ namespace eStoreAPI
 
             app.UseAuthorization();
 
+            app.UseODataBatching();
 
             app.MapControllers();
 
