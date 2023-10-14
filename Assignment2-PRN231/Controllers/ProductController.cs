991d529a4c1c903600164d7de9c438181f7fcaef
hntz123 <hieunthe160326@fpt.edu.vn>
2023-10-14 19:57:26 +0700

    product crud search

---
+++ Assignment2-PRN231/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using Assignment2_PRN231.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Formatter;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Results;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment2_PRN231.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ODataController
+    {
+        private readonly EStoreContext context;
+
+        public ProductController(EStoreContext _context)
+        {
+            context = _context;
+        }
+        [EnableQuery]
+        [HttpGet]
+        public IQueryable<Product> Get()
+        {
+            return context.Products;
+        }
+
+        [EnableQuery]
+        [HttpGet("{key}")]
+        public IActionResult Get(int key)
+        {
+            return Ok(context.Products.FirstOrDefault(a => a.ProductId == key));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+
+            return Created(product);
+        }
+
+        [HttpPut("{key}")]
+        public IActionResult Put([FromODataUri] int key, [FromBody] Product product)
+        {
+
+          Product p = context.Products.FirstOrDefault(a => a.ProductId == key);
+            if(p != null)
+            {
+                context.Entry(product).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+
+            return Updated(product);
+        }
+
+        [HttpDelete("{key}")]
+        public IActionResult Delete([FromODataUri] int key)
+        {
+            Product p = context.Products.FirstOrDefault(a => a.ProductId == key);
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            context.Products.Remove(p);
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+}
---
+++ Assignment2-PRN231/Program.cs
@@ -0,0 +1,36 @@
+using Assignment2_PRN231.Models;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddOData(option => option.Select().Filter().Count().OrderBy().Expand());
+builder.Services.AddDbContext<EStoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDB"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
