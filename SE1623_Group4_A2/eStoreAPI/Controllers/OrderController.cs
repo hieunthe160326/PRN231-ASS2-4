e73fefd9bb22d17b036884170b6e693f45cf9d15
VuHoangAnhHE163109 <anhvhhe163109@fpt.edu.vn>
2023-10-14 23:56:46 +0700

    Update_OrderController

--- SE1623_Group4_A2/eStoreAPI/Controllers/OrderController.cs
+++ SE1623_Group4_A2/eStoreAPI/Controllers/OrderController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Results;
 using Microsoft.AspNetCore.OData.Routing.Attributes;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace eStoreAPI.Controllers
 {
@@ -12,42 +14,38 @@ namespace eStoreAPI.Controllers
     [ApiController]
     public class OrderController : ODataController
     {
-        private readonly EStoreContext context;
+        private readonly EStoreContext _context;
 
         public OrderController(EStoreContext context)
         {
-            this.context = context;
+            this._context = context;
         }
 
         [EnableQuery]
-        [HttpGet]
-        public IQueryable<Order> GetOrders()
+        public IQueryable<Order> Get()
         {
-            return context.Orders.AsQueryable();
+            return _context.Orders;
         }
 
         [EnableQuery]
-        [HttpGet("{id}")]
-        public SingleResult<Order> GetOrder([FromODataUri] int id)
+        public SingleResult<Order> Get([FromODataUri] int key)
         {
-            var order = context.Orders.Where(o => o.OrderId == id);
-            return SingleResult.Create(order);
+            IQueryable<Order> result = _context.Orders.Where(m => m.OrderId == key);
+            return SingleResult.Create(result);
         }
 
-        [EnableQuery]
-        [HttpDelete("{id}")]
-        public IActionResult DeleteOrder([FromODataUri] int id)
+        public IActionResult Delete([FromODataUri] int key)
         {
-            var order = context.Orders.Find(id);
+            var order = _context.Orders.FirstOrDefault(m => m.MemberId == key);
             if (order == null)
             {
                 return NotFound();
             }
 
-            context.Orders.Remove(order);
-            context.SaveChanges();
+            _context.Orders.Remove(order);
+            _context.SaveChanges();
 
-            return NoContent();
+            return StatusCode((int)HttpStatusCode.NoContent);
         }
     }
 }
