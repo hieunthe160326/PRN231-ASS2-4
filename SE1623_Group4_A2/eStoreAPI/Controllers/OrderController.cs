9ee678b122544c0c8b0d36f97930df2ae8f0c72d
VuHoangAnhHE163109 <anhvhhe163109@fpt.edu.vn>
2023-10-15 01:33:22 +0700

    Update_RDOrder

--- SE1623_Group4_A2/eStoreAPI/Controllers/OrderController.cs
+++ SE1623_Group4_A2/eStoreAPI/Controllers/OrderController.cs
@@ -22,18 +22,21 @@ namespace eStoreAPI.Controllers
         }
 
         [EnableQuery]
+        [HttpGet]
         public IQueryable<Order> Get()
         {
             return _context.Orders;
         }
 
         [EnableQuery]
+        [HttpGet("{key}")]
         public SingleResult<Order> Get([FromODataUri] int key)
         {
             IQueryable<Order> result = _context.Orders.Where(m => m.OrderId == key);
             return SingleResult.Create(result);
         }
 
+        [HttpDelete("{key}")]
         public IActionResult Delete([FromODataUri] int key)
         {
             var order = _context.Orders.FirstOrDefault(m => m.MemberId == key);
